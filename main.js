67c76274aa67da72dadf89a76d4c2cc6bb9c016d
Brian Gill <bgag1111@gmail.com>
2023-03-25 16:42:50 +1300

    Create main.js

---
+++ main.js
@@ -0,0 +1,40 @@
+// Service worker registration
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker.register('/sw.js')
+      .then(registration => {
+        console.log('Service worker registered:', registration);
+      })
+      .catch(error => {
+        console.log('Service worker registration failed:', error);
+      });
+  });
+}
+
+// Prompt user to install PWA
+window.addEventListener('beforeinstallprompt', event => {
+  // Prevent Chrome 67 and earlier from automatically showing the prompt
+  event.preventDefault();
+  // Stash the event so it can be triggered later
+  window.deferredPrompt = event;
+  // Show install button
+  const installButton = document.getElementById('install-button');
+  installButton.style.display = 'block';
+  installButton.addEventListener('click', () => {
+    // Show the prompt
+    window.deferredPrompt.prompt();
+    // Wait for user to respond to the prompt
+    window.deferredPrompt.userChoice.then(choiceResult => {
+      // Clear the deferredPrompt so it can be shown again
+      window.deferredPrompt = null;
+      // Hide the install button
+      installButton.style.display = 'none';
+    });
+  });
+});
+
+// Hide install button if PWA is already installed
+window.addEventListener('appinstalled', () => {
+  const installButton = document.getElementById('install-button');
+  installButton.style.display = 'none';
+});
